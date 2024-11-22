ef0003bbe81289d7b3df290bfdc0f1dceea7f45a
sabrinaz35 <sj.zuurbier@gmail.com>
2024-11-22 11:04:29 +0100

    proberen hamburger menu

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,22 @@
 // JavaScript Document
 console.log("hi");
+
+
+// Tevoorschijn laten komen van het menu
+var menuButton = document.querySelector("header > button");
+var deNav = document.querySelector("nav");
+
+menuButton.addEventListener("click",openMenu);
+
+function openMenu(){
+    deNav.classList.add("menuZichtbaar");
+}
+
+// Dichtklappen van het menu
+var closeMenuButton = document.querySelector("ul:nth-of-type(1) button");
+
+closeMenuButton.addEventListener("click", closeMenu);
+
+function closeMenu(){
+    deNav.classList.remove("menuZichtbaar");
+}
